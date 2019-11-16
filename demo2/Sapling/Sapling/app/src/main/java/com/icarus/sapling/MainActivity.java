a856afa7567453db63bd9f045cbdaad1832f8d85
Alexander Gonzales <acgonzales42@gmail.com>
2019-11-16 16:04:01 -0700

    Working on Library

--- demo2/Sapling/Sapling/app/src/main/java/com/icarus/sapling/MainActivity.java
+++ demo2/Sapling/Sapling/app/src/main/java/com/icarus/sapling/MainActivity.java
@@ -16,12 +16,15 @@ import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
+    public ArrayList<Plant> library;
+    public ArrayList<Plant> gardenPlants;
+    public ArrayList<Plant> recommendedPlants;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         try {
-            ArrayList<Plant> library = JSONReader.parseLibrary("plantdata.json");
+            library = JSONReader.parseLibrary("plantdata.json");
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -30,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_library, R.id.navigation_notifications)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
--- demo2/Sapling/Sapling/app/src/main/java/com/icarus/sapling/ui/plantLibrary/PlantLibraryFragment.java
+++ demo2/Sapling/Sapling/app/src/main/java/com/icarus/sapling/ui/plantLibrary/PlantLibraryFragment.java
@@ -1,9 +1,14 @@
 package com.icarus.sapling.ui.plantLibrary;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -14,24 +19,51 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.icarus.sapling.R;
 
+import java.util.ArrayList;
+
 public class PlantLibraryFragment extends Fragment
 {
+    private PlantLibraryViewModel libraryViewModel;
+    //public static TextView data;
+    public static ListView plantlist;
+    public static ArrayList<String> plantarray = new ArrayList<String>();
+    Button click;
+    public static ArrayAdapter<String> listViewAdapter;
 
-    private PlantLibraryViewModel dashboardViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        dashboardViewModel = ViewModelProviders.of(this).get(PlantLibraryViewModel.class);
-
+        libraryViewModel = ViewModelProviders.of(this).get(PlantLibraryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_library, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(this,
-                new Observer<String>() {
+
+        //data = (TextView) root.findViewById(R.id.fetcheddata);
+
+
+        // find by id plantlist view
+        plantlist = (ListView) root.findViewById(R.id.plantList);
+
+        // Adapter part
+
+        //ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1,plantarray);
+        listViewAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1,plantarray);
+        // Set adapter for plantlist
+        plantlist.setAdapter(listViewAdapter);
+
+        // Click find by id
+        click = (Button) root.findViewById(R.id.button);
+
+        // Button click Listener
+        click.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+
+                fetchData process = new fetchData();
+                process.execute();
+
             }
+
         });
+
         return root;
     }
 }
