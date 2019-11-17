da8b77692727d8b71c7218e371e826d053dc1eec
Jose Pacheco <pyro_dragonet@yahoo.com>
2019-11-16 17:42:06 -0700

    Head fixed

--- demo2/Sapling/Sapling/app/src/main/java/com/icarus/sapling/MainActivity.java
+++ demo2/Sapling/Sapling/app/src/main/java/com/icarus/sapling/MainActivity.java
@@ -33,11 +33,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-<<<<<<< HEAD
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_recommendations)
-=======
-                R.id.navigation_home, R.id.navigation_library, R.id.navigation_notifications)
->>>>>>> a856afa7567453db63bd9f045cbdaad1832f8d85
+                R.id.navigation_home, R.id.navigation_library, R.id.navigation_recommendations)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
